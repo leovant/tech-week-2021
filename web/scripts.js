9cc33c0e514645c10b973ab2f62d6d52d373b914
Leovan Tavares <leovant@gmail.com>
2021-08-11 19:52:34 -0300

    :sparkles: Limpa formulario e valida nome

---
+++ web/scripts.js
@@ -0,0 +1,36 @@
+// Obter referência dos campos
+const personName = document.getElementById("person-name")
+const personEmail = document.getElementById("person-email")
+const personBirth = document.getElementById("person-birth")
+const buttonClear = document.getElementById("form-clear")
+
+/**
+ * Limpa os campos do formulário
+ */
+function clearForm(event) {
+  event.preventDefault()
+
+  personName.value = ''
+  personEmail.value = ''
+  personBirth.value = ''
+}
+
+buttonClear.addEventListener('click', clearForm)
+
+/**
+ * Valida o nome: deve aceitar apenas letras
+ */
+function validatePersonName() {
+  const name = String(personName.value).trim()
+
+  if (!name) {
+    return personName.classList.add('invalid')
+  }
+
+  const valid = /^[a-zA-Z]+$/.test(name)
+
+  if (valid) {
+    return personName.classList.remove('invalid')
+  }
+  return personName.classList.add('invalid')
+}
