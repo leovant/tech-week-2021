919743c62fec54e87a5018981a42dd3dbd06e832
Leovan Tavares <leovant@gmail.com>
2021-08-11 10:28:20 -0300

    :sparkles: Implementa rota para receber dados do formulario

--- api/api.js
+++ api/api.js
@@ -2,8 +2,25 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/ping', (req, res) => {
   res.send('Pong')
+})
+
+/*
+  Pode ser chamada com o comando curl:
+  curl --request POST \
+  --url http://localhost:3000/person \
+  --header 'Content-Type: application/x-www-form-urlencoded' \
+  --data 'name=Leovan Tavares' \
+  --data email=leovant@gmail.com \
+  --data birthDate=04/09/1984
+ */
+app.post('/person', (req, res) => {
+  const { name, email, birthDate } = req.body
+  
+  res.json({ name, email, birthDate })
 })
 
 app.listen(port, () => {
