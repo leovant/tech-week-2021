5941dd5ffd1ffa01c9f0c05367206e014d0da556
Leovan Tavares <leovant@gmail.com>
2021-08-11 20:55:54 -0300

    :construction: API para gravar os dados

--- api/api.js
+++ api/api.js
@@ -1,9 +1,32 @@
+const fs = require('fs')
 const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/ping', (req, res) => {
   res.send('Pong')
+})
+
+app.post('/person', function(request, response) {
+  const { name, email, birthDate } = request.body
+
+  console.log('New person received:')
+  console.table([ name, email, birthDate ])
+
+  try {
+    const file = fs.createWriteStream('people.txt', { flag: 'a' }) // a flag 'a' é para incluir dados, mantendo os anteriores
+    const line = `${String(name).padEnd(100, ' ')}${String(email).padEnd(30, ' ')}${String(birthDate).padStart(15, ' ')}\n`
+
+    file.write(line)
+    file.end()
+  } catch (e) {
+    console.error(e.message)
+    return response.status(500).json({ message: e.message })
+  }
+
+  response.status(201).json({ name, email, birthDate })
 })
 
 app.listen(port, () => {
